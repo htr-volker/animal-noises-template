d3387e6484820e87cad505c93c454acbcdc5123a
gtegbe <gtegbe@qa.com>
2021-06-03 17:19:05 +0000

    made some changes

--- jenkins/setup-docker.sh
+++ jenkins/setup-docker.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#Install docker
-
-curl https://get.docker.com | sudo bash
-
-# add jenkins to docker group
-sudo usermod -aG docker jenkins
-
-
 #Log in to docker
 docker login -u $DOCKER_LOGIN_USR -p $DOCKER_LOGIN_PSW
 
