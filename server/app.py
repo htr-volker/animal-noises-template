23c40c3fcbc4ae0826acadaec9eb7aa52e13c445
htr-volker <htr.volker@gmail.com>
2021-06-08 11:08:01 +0100

    animal noises tested and dockerised

--- server/app.py
+++ server/app.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 db = SQLAlchemy(app)
-db.create_all()
 
 class Animals(db.Model):
-    id = db.Column(db.Integer, primay_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(50))
     noise = db.Column(db.String(50))
 
+db.create_all()
+
 @app.route('/')
 def home():
-    animal = requests.get("http://animal_api/get_animal").text
-    noise = requests.post("http://animal_api/get_noise", data=animal).text
+    animal = requests.get("http://animal_api:5000/get_animal").text
+    noise = requests.post("http://animal_api:5000/get_noise", data=animal).text
     
-    animals = Animals.query.all()
+    animals = Animals.query.order_by(desc(Animals.id)).limit(5).all()
     db.session.add(Animals(type = animal, noise = noise))
     db.session.commit()
     
---
+++ server/tests/test_server.py
@@ -0,0 +1,39 @@
+from flask import url_for
+from flask_testing import TestCase
+from app import app, db
+import requests_mock
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(
+            SQLALCHEMY_DATABASE_URI="sqlite:///test.db",
+            DEBUG=True
+        )
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+class TestHome(TestBase):
+    def test_home_get(self):
+        with requests_mock.Mocker() as m:
+            m.get("http://animal_api:5000/get_animal", text='pig')
+            m.post("http://animal_api:5000/get_noise", text='oink')
+            response = self.client.get(url_for("home"))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'The pig goes oink', response.data)
+    
+    def test_db(self):
+        with requests_mock.Mocker() as m:
+            test_cases = [("pig", "oink"), ("horse", "neigh")]
+            for case in test_cases:
+                m.get("http://animal_api:5000/get_animal", text=case[0])
+                m.post("http://animal_api:5000/get_noise", text=case[1])
+                response = self.client.get(url_for("home"))
+            self.assertIn(b'The pig goes oink', response.data)
+            self.assertIn(b'The horse goes neigh', response.data)
