7a2895c4ceb75ac9236cef4fc88f35860c983fdc
Harry <htr.volker@gmail.com>
2021-06-08 15:18:55 +0000

    fixing coverage

--- animal_api_noise/app.py
+++ animal_api_noise/app.py
@@ -12,5 +12,4 @@ def get_noise():
     }
     return noises[request.data.decode('utf-8')]
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__": app.run(host="0.0.0.0", port=5000, debug=True)
--- animal_api_type/app.py
+++ animal_api_type/app.py
@@ -7,5 +7,4 @@ app = Flask(__name__)
 def get_animal():
     return random.choice(["pig", "cow", "horse"])
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__": app.run(host="0.0.0.0", port=5000, debug=True)
