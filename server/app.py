937a520154d5f1438964d22011dfb30354267e4d
Harry <htr.volker@gmail.com>
2021-06-08 10:51:55 +0000

    jenkinsfile plan added

--- server/app.py
+++ server/app.py
@@ -4,25 +4,18 @@ from sqlalchemy import desc
 import requests
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-class Animals(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(50))
-    noise = db.Column(db.String(50))
 
-db.create_all()
+
+# db.create_all()
 
 @app.route('/')
 def home():
     animal = requests.get("http://animal_api:5000/get_animal").text
     noise = requests.post("http://animal_api:5000/get_noise", data=animal).text
-    
-    animals = Animals.query.order_by(desc(Animals.id)).limit(5).all()
-    db.session.add(Animals(type = animal, noise = noise))
-    db.session.commit()
     
     return render_template("index.html", animal=animal, noise=noise, animals=animals)
 
