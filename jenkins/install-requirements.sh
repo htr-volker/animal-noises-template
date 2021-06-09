70974b802d2b3a7fb3ef842f100efb1bd61ad059
Harry <htr.volker@gmail.com>
2021-06-09 11:40:15 +0000

    added ansible config to Jenkinsfile

--- jenkins/install-requirements.sh
+++ jenkins/install-requirements.sh
@@ -12,5 +12,13 @@ version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | 
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Install Ansible
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python3-pip -y
+pip3 install --user ansible
+ansible --version
+
 # Docker login
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
