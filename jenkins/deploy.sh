90f13b0393da9437b55a488625b6d9adad51cd75
Harry <htr.volker@gmail.com>
2021-06-10 09:38:15 +0000

    added database uri env var

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,4 +4,7 @@
 scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@manager:/home/jenkins/docker-compose.yaml
 
 # docker stack deploy
-ssh -i ~/.ssh/ansible_id_rsa jenkins@manager "docker stack deploy --compose-file docker-compose.yaml animal_noises"
+ssh -i ~/.ssh/ansible_id_rsa jenkins@manager << EOF
+    export DATABASE_URI=${DATABASE_URI}
+    docker stack deploy --compose-file docker-compose.yaml animal_noises
+EOF
